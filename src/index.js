a12703248391975b41d7124aec872e7e84adc7b2
Maksym <morenko.maxim@gmail.com>
2023-08-01 03:24:02 +0300

    init: start reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
+    let arr = (``+Math.abs(n)).split``;
+    let left = 0;
+    let right = arr.length - 1;
   
+    while (left < right) {
+       const temp = arr[left];
+       arr[left] = arr[right];
+       arr[right] = temp;
+       left++;
+       right--;
+    }
+    return +arr.join``;
 }
